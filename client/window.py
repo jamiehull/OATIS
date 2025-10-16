58e20b65d5fb1521129912807ba3ca650f98e7ea
jamiehull <72918916+jamiehull@users.noreply.github.com>
2025-10-16 16:55:59 +0100

    Moved Matrix functions to Modules

--- client/window.py
+++ client/window.py
@@ -11,6 +11,7 @@ from modules.osc import *
 from modules.tcp import TCP_Client
 from modules.common import *
 from modules.osc import *
+from modules.matrix_operations import *
 import threading
 from time import sleep
 import json
@@ -214,7 +215,7 @@ class Window:
             print(f"Layout Total Rows:{total_rows}, height:{row_size}")
 
             #Find the display secions in the layout matrix and their top_left_coordinate
-            display_section_dict :dict = self.find_display_sections(self.grid)
+            display_section_dict :dict = find_display_sections(self.grid)
 
             #List to hold display surfaces
             self.display_surface_list = []
@@ -222,7 +223,7 @@ class Window:
             #Find width and height of each display section
             for display_section_id in display_section_dict:
                 top_left_coord = display_section_dict.get(display_section_id)
-                width, height = self.find_display_section_dimensions(display_section_id, top_left_coord, total_columns, total_rows)
+                width, height = find_display_section_dimensions(self.grid, display_section_id, top_left_coord, total_columns, total_rows)
 
                 #Translate matrix coordinate to actual screen coordinates
                 display_section_matrix_coordinates = display_section_dict.get(display_section_id)
@@ -234,7 +235,6 @@ class Window:
 
                 print(f"Display Section:{display_section_id} Martix Width:{width}, Matrix Height:{height}, Screen Width:{display_section_screen_width}, Screen Height:{display_section_screen_height}") 
                 print(f"Top Left Matrix Coordinate:{display_section_matrix_coordinates}, Top Left Screen Coordinate:{display_section_screen_coordinates}")
-
 
                 #Create each surface and add to blit
                 surface = pygame.Surface((display_section_screen_width, display_section_screen_height))
@@ -277,35 +277,6 @@ class Window:
         self.fullscreen_identify_slate = Identify_Slate(self.surface_identify_slate)
         self.__add_widget_to_render(-4, self.fullscreen_identify_slate)
 
-    def find_display_section_dimensions(self, display_section_id:int, top_left_coord:tuple, total_columns:int, total_rows:int):
-        """Finds the dimension of a display section"""
-        width=0
-        height=0
-
-        #Origin to start scanning from
-        x=top_left_coord[0]
-        y=top_left_coord[1]
-
-        #Finding width
-        for i in range(total_columns - top_left_coord[0]):
-            if self.grid[y][x] == display_section_id:
-                #print(f"Position:{x},{y}")
-                width += 1
-                x += 1
-
-        #Origin to start scanning from
-        x=top_left_coord[0]
-        y=top_left_coord[1]
-
-        #Finding height
-        for i in range(total_rows - top_left_coord[1]):
-            if self.grid[y][x] == display_section_id:
-                #print(f"Position:{x},{y}")
-                height += 1
-                y += 1
-
-        return width, height
-
     def validate_layout_matrix(self, layout_matrix:list) -> bool:
         """Validates an input layout matrix"""
         valid_matrix = True
@@ -317,27 +288,6 @@ class Window:
                 valid_matrix = False
 
         return valid_matrix
-    
-    def find_display_sections(self, layout_matrix):
-        """Finds the display secions in the layout matrix and their top_left_coordinate, returning a dict.
-        Keys are display section id's, values are the top_left_coordinate as a tuple."""
-        #Working Dict storing section id's and their top left coordinate
-        display_section_id_dict = {}
-
-        #Origin to start scanning from
-        x=0
-        y=0
-
-        #Loop to find each area id and its origin
-        for row in layout_matrix:
-            for column in row:
-                if column not in display_section_id_dict:
-                    display_section_id_dict[column] = (x, y)
-                x += 1 #Advance to next column
-            y += 1 #Advance to next row
-            x = 0 #Reset column position to 0 as we are in a new row
-
-        return display_section_id_dict
 
     def add_widget_to_surface(self, widget, surface_id, **args):
         """Adds a widget to a surface, optional args for widget configuration"""
---
+++ modules/matrix_operations.py
@@ -0,0 +1,49 @@
+def find_display_sections(self, layout_matrix):
+        """Finds the display secions in the layout matrix and their top_left_coordinate, returning a dict.
+        Keys are display section id's, values are the top_left_coordinate as a tuple."""
+        #Working Dict storing section id's and their top left coordinate
+        display_section_id_dict = {}
+
+        #Origin to start scanning from
+        x=0
+        y=0
+
+        #Loop to find each area id and its origin
+        for row in layout_matrix:
+            for column in row:
+                if column not in display_section_id_dict:
+                    display_section_id_dict[column] = (x, y)
+                x += 1 #Advance to next column
+            y += 1 #Advance to next row
+            x = 0 #Reset column position to 0 as we are in a new row
+
+        return display_section_id_dict
+
+def find_display_section_dimensions(self, layoput_matrix:list, display_section_id:int, top_left_coord:tuple, total_columns:int, total_rows:int):
+        """Finds the dimension of a display section"""
+        width=0
+        height=0
+
+        #Origin to start scanning from
+        x=top_left_coord[0]
+        y=top_left_coord[1]
+
+        #Finding width
+        for i in range(total_columns - top_left_coord[0]):
+            if layoput_matrix[y][x] == display_section_id:
+                #print(f"Position:{x},{y}")
+                width += 1
+                x += 1
+
+        #Origin to start scanning from
+        x=top_left_coord[0]
+        y=top_left_coord[1]
+
+        #Finding height
+        for i in range(total_rows - top_left_coord[1]):
+            if layoput_matrix[y][x] == display_section_id:
+                #print(f"Position:{x},{y}")
+                height += 1
+                y += 1
+
+        return width, height
